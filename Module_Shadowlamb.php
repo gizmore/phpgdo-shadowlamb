8b436b682aa0805f3d8325df7d31d711da4aa6fd
gizmore <gizmore@wechall.net>
2023-03-21 19:24:08 +0100

    '7.0.2-r1765 Hailing Hermelin'

--- Module_Shadowlamb.php
+++ Module_Shadowlamb.php
@@ -5,18 +5,18 @@ use GDO\Core\GDO_Module;
 
 /**
  * A website for the shadowlamb IRC MMO RPG.
- * 
- * @author gizmore
+ *
  * @version 7.0.2
+ * @author gizmore
  */
 final class Module_Shadowlamb extends GDO_Module
 {
-	
+
 	public int $priority = 100;
-	
+
 	public function isSiteModule(): bool
 	{
 		return true;
 	}
-	
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # phpgdo-shadowlamb
+
 A small website for the Shadowlamb IRCMMORPG. Browse players, show statistics and a manual.
